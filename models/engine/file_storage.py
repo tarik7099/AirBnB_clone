3ce6d9201e89bc6d88c0eb45cc6147a42f2e2e7c
tarik khadr <tarikkhadri11@gmail.com>
2024-02-08 21:05:53 +0100

    all task

---
+++ models/engine/file_storage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Module: file_storage.py
+
+Defines a `FileStorage` class.
+"""
+import os
+import json
+from models.base_model import BaseModel
+from models.user import User
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    CLASSES = {
+        'BaseModel' : BaseModel, 
+        'User' : User
+    } 
+
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file"""
+        serialized_objs = {}
+        for key, obj in self.__objects.items():
+            serialized_objs[key] = obj.to_dict()
+        with open(self.__file_path, "w") as file:
+            json.dump(serialized_objs, file)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, "r") as file:
+                serialized_objs = json.load(file)
+                for key, value in serialized_objs.items():
+                    class_name, obj_id = key.split('.')
+                    module_name = class_name.lower()  # Assuming module names are lowercase
+                    class_ = globals()[class_name]  # Assuming classes are defined globally
+                    self.__objects[key] = class_(**value)
+        except FileNotFoundError:
+            pass
