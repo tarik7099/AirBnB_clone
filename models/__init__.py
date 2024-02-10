04baa24e6caa5bc1b571de3967f022963ab21515
tarik khadr <tarikkhadri11@gmail.com>
2024-02-10 17:11:27 +0100

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""
-Module: __init__.py
-"""
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
--- models/engine/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""
-Module: __init__.py
-"""
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
