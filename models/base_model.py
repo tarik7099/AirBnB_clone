d9b497b4f4dbab1bbe3a5b79dfa2c1114bb9e90d
tarik khadr <tarikkhadri11@gmail.com>
2024-02-10 11:58:34 +0100

    fix storage

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,13 @@ import models
 import uuid
 from datetime import datetime
 
-from models.__init__ import storage
 
-class BaseModel:
+class BaseModel():
+    """
+    Base class which defines all common
+    attributes/methods for other classes
+    """
+
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance"""
         if kwargs:
@@ -21,18 +25,32 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-            storage.new(self)
-            storage.save()
+            models.storage.new(self)
+            models.storage.save()
+    def __str__(self):
+        """
+        Returns the string representation
+        of the instance
+        """
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update updated_at attribute and save to storage"""
-        self.updated_at = datetime.datetime.now()
-        storage.save()
+        """
+        updates the public instance attribute
+        updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """Return dictionary representation of BaseModel"""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
-        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
-        return obj_dict
+        """
+        returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        dict = {**self.__dict__}
+        dict['__class__'] = type(self).__name__
+        dict['created_at'] = dict['created_at'].isoformat()
+        dict['updated_at'] = dict['updated_at'].isoformat()
+
+        return dict
