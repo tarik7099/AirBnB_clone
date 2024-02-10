8c50d98fddff300cf70b5387761d40b69c218603
tarik khadr <tarikkhadri11@gmail.com>
2024-02-10 12:28:39 +0100

    fix storage

--- models/base_model.py
+++ models/base_model.py
@@ -48,8 +48,9 @@ class BaseModel():
         updates the public instance attribute
         updated_at with the current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
         models.storage.save()
+    
     def to_dict(self):
         """Return dictionary representation of BaseModel"""
         obj_dict = self.__dict__.copy()
