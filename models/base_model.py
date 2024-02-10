cf4e61abcc52049ad5358f924d03be5ce1bbe93f
tarik khadr <tarikkhadri11@gmail.com>
2024-02-10 17:04:59 +0100

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -14,19 +14,24 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        """Initialization of BaseModel class."""
+        """
+        instantiates an object with its
+        attributes
+        """
         if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, value)
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
             models.storage.new(self)
-   
+
     def __str__(self):
         """
         Returns the string representation
@@ -53,3 +58,11 @@ class BaseModel:
         dict_repr['created_at'] = self.created_at.isoformat()
         dict_repr['updated_at'] = self.updated_at.isoformat()
         return dict_repr
+
+
+# Update models/__init__.py to create a unique FileStorage instance for your application
+# Update models/base_model.py to link your BaseModel to FileStorage by using the variable storage
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
