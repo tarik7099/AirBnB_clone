09f172819bbada39c1a8f9e706f4b3ee9a40067e
tarik khadr <tarikkhadri11@gmail.com>
2024-02-10 11:49:28 +0100

    fix storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,19 @@
+#!/usr/bin/python3
+"""
+Module: file_storage.py
+
+Defines a `FileStorage` class.
+"""
 import os
 import json
 import datetime
 from models.base_model import BaseModel
+from models.user import User
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
+
 
     def all(self):
         """Returns the dictionary __objects"""
@@ -20,7 +28,7 @@ class FileStorage:
         """Serializes __objects to the JSON file"""
         serialized_objs = {}
         for key, obj in self.__objects.items():
-            serialized_objs[key] = obj.to_dict()
+            serialized_objs[key] = obj.to_dicta()
         with open(self.__file_path, "w") as file:
             json.dump(serialized_objs, file)
 
@@ -36,4 +44,41 @@ class FileStorage:
                     self.__objects[key] = class_(**value)
         except FileNotFoundError:
             pass
-
+    
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
