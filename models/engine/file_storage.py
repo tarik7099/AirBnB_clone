1efda188c519ce9d45fee132b4f1935edc14d6d5
tarik khadr <tarikkhadri11@gmail.com>
2024-02-11 00:30:25 +0100

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,3 +59,7 @@ class FileStorage:
                     self.__objects[key] = class_(**value)
         except FileNotFoundError:
             pass
+
+from models.engine import file_storage
+
+storage = file_storage.FileStorage()
