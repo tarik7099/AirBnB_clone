ee52655a3598877d782a410706be3d1cd65946f3
tarik khadr <tarikkhadri11@gmail.com>
2024-02-10 12:34:59 +0100

    fix storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,55 @@
 #!/usr/bin/python3
 """
-Module: base.py
+Module: file_storage.py
+
+Defines a `FileStorage` class.
 """
-import models
-import uuid
-from datetime import datetime
+import os
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.review import Review
+from models.amenity import Amenity
+from models.place import Place
 
 
-class BaseModel():
+class FileStorage():
 
-    def __init__(self, *args, **kwargs):
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
         """
-        instatiates an object with it's
-        attributes
+        returns the dictionary __objects
         """
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-            return
+        return FileStorage.__objects
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        models.storage.new(self)
-
-    def __str__(self):
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+    def new(self, obj):
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        self.updated_at = datetime.now()
-        models.storage.save()
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(
+                {k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
 
-    def to_dict(self):
-        dict = {**self.__dict__}
-        dict['__class__'] = type(self).__name__
-        dict['created_at'] = dict['created_at'].isoformat()
-        dict['updated_at'] = dict['updated_at'].isoformat()
-
-        return dict
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, "r") as file:
+                serialized_objs = json.load(file)
+                for key, value in serialized_objs.items():
+                    class_name, obj_id = key.split('.')
+                    module_name = class_name.lower()  # Assuming module names are lowercase
+                    class_ = globals()[class_name]  # Assuming classes are defined globally
+                    self.__objects[key] = class_(**value)
+        except FileNotFoundError:
+            pass
