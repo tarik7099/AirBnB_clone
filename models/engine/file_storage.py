22ea732d26e6eb55b462a0c70e4684ab23274b2a
tarik khadr <tarikkhadri11@gmail.com>
2024-02-10 11:52:39 +0100

    fix storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,25 +4,34 @@ import datetime
 from models.base_model import BaseModel
 
 class FileStorage:
+    """ serializes instances to json """
+
+
     __file_path = "file.json"
     __objects = {}
+
 
     def all(self):
         """Returns the dictionary __objects"""
         return self.__objects
 
+
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = obj.__class__.__name__ + "." + str(obj.id)
         self.__objects[key] = obj
+
 
     def save(self):
         """Serializes __objects to the JSON file"""
+
+
         serialized_objs = {}
         for key, obj in self.__objects.items():
             serialized_objs[key] = obj.to_dict()
         with open(self.__file_path, "w") as file:
             json.dump(serialized_objs, file)
+
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
